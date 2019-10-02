27b2e57f2849e67dc513553d4b69dab02a7c41f3
Mike Reposa <mreposa@cra.com>
2019-10-02 11:06:38 -0400

    Updates for new data

--- JDA.py
+++ JDA.py
@@ -13,16 +13,40 @@ RIGHT = 3
 
 def train_model(training_data_files, model_file):
 	base_ch1 = genfromtxt(training_data_files[0][0], delimiter=',')
+	base_ch1 = base_ch1[:,0]
+	base_ch1 = base_ch1[~np.isnan(base_ch1)]
+
 	base_ch2 = genfromtxt(training_data_files[0][1], delimiter=',')
+	base_ch2 = base_ch2[:,0]
+	base_ch2 = base_ch2[~np.isnan(base_ch2)]
+
 	base_ch3 = genfromtxt(training_data_files[0][2], delimiter=',')
+	base_ch3 = base_ch3[:,0]
+	base_ch3 = base_ch3[~np.isnan(base_ch3)]
 
 	front_ch1 = genfromtxt(training_data_files[1][0], delimiter=',')
+	front_ch1 = front_ch1[:,0]
+	front_ch1 = front_ch1[~np.isnan(front_ch1)]
+
 	front_ch2 = genfromtxt(training_data_files[1][1], delimiter=',')
+	front_ch2 = front_ch2[:,0]
+	front_ch2 = front_ch2[~np.isnan(front_ch2)]
+
 	front_ch3 = genfromtxt(training_data_files[1][2], delimiter=',')
+	front_ch3 = front_ch3[:,0]
+	front_ch3 = front_ch3[~np.isnan(front_ch3)]
 
 	right_ch1 = genfromtxt(training_data_files[2][0], delimiter=',')
+	right_ch1 = right_ch1[:,0]
+	right_ch1 = right_ch1[~np.isnan(right_ch1)]
+
 	right_ch2 = genfromtxt(training_data_files[2][1], delimiter=',')
+	right_ch2 = right_ch2[:,0]
+	right_ch2 = right_ch2[~np.isnan(right_ch2)]
+
 	right_ch3 = genfromtxt(training_data_files[2][2], delimiter=',')
+	right_ch3 = right_ch3[:,0]
+	right_ch3 = right_ch3[~np.isnan(right_ch3)]
 
 	model = ""
 	model += str(np.mean(base_ch1)) + ","
@@ -37,41 +61,23 @@ def train_model(training_data_files, model_file):
 	model += str(np.mean(right_ch2)) + ","
 	model += str(np.mean(right_ch3)) + "\n"
 
-	base_shift_ch12 = base_ch1 - base_ch2
-	base_shift_ch13 = base_ch1 - base_ch3
-	base_shift_ch23 = base_ch2 - base_ch3
 
-	front_shift_ch12 = front_ch1 - front_ch2
-	front_shift_ch13 = front_ch1 - front_ch3
-	front_shift_ch23 = front_ch2 - front_ch3
+	model += str(np.mean((base_ch2 + base_ch3) / 2)) + ","
+	model += str(np.mean((front_ch2 + front_ch3) / 2)) + ","
+	model += str(np.mean((right_ch2 + right_ch3) / 2)) + "\n"
+	
 
-	right_shift_ch12 = right_ch1 - right_ch2
-	right_shift_ch13 = right_ch1 - right_ch3
-	right_shift_ch23 = right_ch2 - right_ch3
+	model += str(normalized_var(base_ch1)) + ","
+	model += str(normalized_var(base_ch2)) + ","
+	model += str(normalized_var(base_ch3)) + "\n"
 
-	model += str(np.mean(base_shift_ch12)) + ","
-	model += str(np.mean(base_shift_ch13)) + ","
-	model += str(np.mean(base_shift_ch23)) + "\n"
+	model += str(normalized_var(front_ch1)) + ","
+	model += str(normalized_var(front_ch2)) + ","
+	model += str(normalized_var(front_ch3)) + "\n"
 
-	model += str(np.mean(front_shift_ch12)) + ","
-	model += str(np.mean(front_shift_ch13)) + ","
-	model += str(np.mean(front_shift_ch23)) + "\n"
-
-	model += str(np.mean(right_shift_ch12)) + ","
-	model += str(np.mean(right_shift_ch13)) + ","
-	model += str(np.mean(right_shift_ch23)) + "\n"
-
-	model += str(normalized_var(base_shift_ch12)) + ","
-	model += str(normalized_var(base_shift_ch13)) + ","
-	model += str(normalized_var(base_shift_ch23)) + "\n"
-
-	model += str(normalized_var(front_shift_ch12)) + ","
-	model += str(normalized_var(front_shift_ch13)) + ","
-	model += str(normalized_var(front_shift_ch23)) + "\n"
-
-	model += str(normalized_var(right_shift_ch12)) + ","
-	model += str(normalized_var(right_shift_ch13)) + ","
-	model += str(normalized_var(right_shift_ch23))
+	model += str(normalized_var(right_ch1)) + ","
+	model += str(normalized_var(right_ch2)) + ","
+	model += str(normalized_var(right_ch3))
 
 	with open(model_file, 'w') as build_model:
 		build_model.write(model)
@@ -105,17 +111,6 @@ def failure_detection(data, model):
 		failure_mode = DISCONNECTION
 		failed_ch = 3
 
-	if failure_mode == NO_FAILURE:
-		max_shift_deviation = 200
-		if (max(np.abs(shift_ch12 - model[3:6, 0])) > max_shift_deviation and max(np.abs(shift_ch13 - model[3:6, 1])) > max_shift_deviation):
-			failure_mode = DISCONNECTION
-			failed_ch = 1
-		elif (max(np.abs(shift_ch12 - model[3:6, 0])) > max_shift_deviation and max(np.abs(shift_ch23 - model[3:6, 2])) > max_shift_deviation):
-			failure_mode = DISCONNECTION
-			failed_ch = 2
-		elif (max(np.abs(shift_ch13 - model[3:6, 1])) > max_shift_deviation and max(np.abs(shift_ch23 - model[3:6, 2])) > max_shift_deviation):
-			failure_mode = DISCONNECTION
-			failed_ch = 3
 
 	if failure_mode == NO_FAILURE:
 		return failure_mode, orientation, failed_ch
@@ -123,26 +118,19 @@ def failure_detection(data, model):
 
 	orientation_differences = np.zeros(3)
 	if failed_ch == 1:
-		orientation_differences[0] = abs(shift_ch23 - model[3][2])
-		orientation_differences[1] = abs(shift_ch23 - model[4][2])
-		orientation_differences[2] = abs(shift_ch23 - model[5][2])
-		orientation = np.argmin(orientation_differences) + 1
+		diff_base = np.abs(data[1] - model[0][1]) + np.abs(data[2] - model[0][2])
+		diff_front = np.abs(data[1] - model[1][1]) + np.abs(data[2] - model[1][2])
+		diff_right = np.abs(data[1] - model[2][1]) + np.abs(data[2] - model[2][2])
 	elif failed_ch == 2:
-		orientation_differences[0] = abs(shift_ch13 - model[3][1])
-		orientation_differences[1] = abs(shift_ch13 - model[4][1])
-		orientation_differences[2] = abs(shift_ch13 - model[5][1])
-		orientation = np.argmin(orientation_differences) + 1
+		diff_base = np.abs(data[0] - model[0][0]) + np.abs(data[2] - model[0][2])
+		diff_front = np.abs(data[0] - model[1][0]) + np.abs(data[2] - model[1][2])
+		diff_right = np.abs(data[0] - model[2][0]) + np.abs(data[2] - model[2][2])
 	elif failed_ch == 3:
-		orientation_differences[0] = abs(shift_ch12 - model[3][0])
-		orientation_differences[1] = abs(shift_ch12 - model[4][0])
-		orientation_differences[2] = abs(shift_ch12 - model[5][0])
-		orientation = np.argmin(orientation_differences) + 1
+		diff_base = np.abs(data[1] - model[0][1]) + np.abs(data[0] - model[0][0])
+		diff_front = np.abs(data[1] - model[1][1]) + np.abs(data[0] - model[1][0])
+		diff_right = np.abs(data[1] - model[2][1]) + np.abs(data[0] - model[2][0])
 
-		if orientation != RIGHT:
-			avg_differences = np.zeros(2)
-			avg_differences[0] = abs(ch1 - model[0][0]) + abs(ch2 - model[0][1])
-			avg_differences[1] = abs(ch1 - model[1][0]) + abs(ch2 - model[1][1])
-			orientation = np.argmin(avg_differences) + 1
+	orientation = np.argmin((diff_base, diff_front, diff_right)) + 1
 
 	return failure_mode, orientation, failed_ch
 
@@ -152,16 +140,13 @@ def adaptation(data, detection_info, model):
 		return data
 
 	orientation = detection_info[1]
-	selected_shifts = model[orientation + 2]
+	failed_ch = detection_info[2]
 
 	adapted_data = data
-	failed_ch = detection_info[2]
 	if failed_ch == 1:
-		adapted_ch_data = ((selected_shifts[0] + data[1]) + (selected_shifts[1] + data[2])) / 2
-	elif failed_ch == 2:
-		adapted_ch_data = ((-selected_shifts[0] + data[0]) + (selected_shifts[2] + data[2])) / 2
+		adapted_ch_data = model[orientation - 1][0]
 	else:
-		adapted_ch_data = ((-selected_shifts[1] + data[0]) + (-selected_shifts[2] + data[1])) / 2
+		adapted_ch_data = model[3][orientation - 1] * 2 - data[2 if failed_ch==2 else 1]
 
 	adapted_data[failed_ch - 1] = adapted_ch_data
 	return adapted_data
@@ -181,14 +166,15 @@ def plot_chs(chs_data, setting):
 	plt.ylabel('Counts')
 	plt.legend()
 	plt.show()
-	plt.savefig(setting + '.png')
+	plt.savefig('result_plots/' + setting + '.png')
+	plt.close()
 
 
 if __name__ == "__main__":
-	root_path = "instrumentation-test-data-master/mkz/data/"
+	root_path = "instrumentation-test-data-master/mkz/new_data/"
 
 	base_path = root_path + "base/"
-	front_path = root_path + "front/"
+	front_path = root_path + "forward/"
 	right_path = root_path + "right/"
 
 	base_normal_path = base_path + "normal_01/"
@@ -215,36 +201,49 @@ if __name__ == "__main__":
 	train_model(training_data_files, model_file)
 	model = genfromtxt(model_file, delimiter=',')
 
+	selected_orientaions = ["base", "forward", "right"]
+	modes = ["normal", "bad", "switched"]
 
-	selected_orientaion = "base"
-	mode = "bad"
-	test_root_path = root_path + selected_orientaion + "/" + mode + "_01/"
-	ch1_testing_path = test_root_path + "data_ch1.csv"
-	ch2_testing_path = test_root_path + "data_ch2.csv"
-	ch3_testing_path = test_root_path + "data_ch3.csv"
+	for selected_orientaion in selected_orientaions:
+		for mode in modes:
+			test_root_path = root_path + selected_orientaion + "/" + mode + "_02/"
+			ch1_testing_path = test_root_path + "data_ch1.csv"
+			ch2_testing_path = test_root_path + "data_ch2.csv"
+			ch3_testing_path = test_root_path + "data_ch3.csv"
 
-	ch1_testing_data = genfromtxt(ch1_testing_path, delimiter=',')
-	ch2_testing_data = genfromtxt(ch2_testing_path, delimiter=',')
-	ch3_testing_data = genfromtxt(ch3_testing_path, delimiter=',')
+			ch1_testing_data = genfromtxt(ch1_testing_path, delimiter=',')
+			ch1_testing_data = ch1_testing_data[:, 0]
+			ch1_testing_data = ch1_testing_data[~np.isnan(ch1_testing_data)]
 
-	testing_data = np.vstack([ch1_testing_data, ch2_testing_data, ch3_testing_data])
-	testing_data = testing_data.T
+			ch2_testing_data = genfromtxt(ch2_testing_path, delimiter=',')
+			ch2_testing_data = ch2_testing_data[:, 0]
+			ch2_testing_data = ch2_testing_data[~np.isnan(ch2_testing_data)]
 
-	adapted_data_set = np.zeros(testing_data.shape)
-	for index in range(len(testing_data)):
-		testing_data_point = np.copy(testing_data[index])
-		detection_info = failure_detection(testing_data_point, model)
-		adapted_data_point = adaptation(testing_data_point, detection_info, model)
-		adapted_data_set[index] = adapted_data_point
+			ch3_testing_data = genfromtxt(ch3_testing_path, delimiter=',')
+			ch3_testing_data = ch3_testing_data[:, 0]
+			ch3_testing_data = ch3_testing_data[~np.isnan(ch3_testing_data)]
 
-	output_file = 'adapted_data.txt'
-	np.savetxt(output_file, adapted_data_set, delimiter=',')
+			testing_data = np.vstack([ch1_testing_data, ch2_testing_data, ch3_testing_data])
+			testing_data = testing_data.T
 
-	# Save somewhere SwRI can access
-	docker_output_dir = '/data/cp2/'
-	os.makedirs(docker_output_dir, exist_ok=True)
-	copyfile(output_file, docker_output_dir + output_file)
-	#plot_chs(adapted_data_set.T, selected_orientaion + '_' + mode)
+			adapted_data_set = np.zeros(testing_data.shape)
+			for index in range(len(testing_data)):
+				testing_data_point = np.copy(testing_data[index])
+				detection_info = failure_detection(testing_data_point, model)
+				adapted_data_point = adaptation(testing_data_point, detection_info, model)
+				adapted_data_set[index] = adapted_data_point
+
+
+			output_file = selected_orientaion + '_' + mode + '_adapted_data.txt'
+			np.savetxt('adaptation/' + output_file, adapted_data_set, delimiter=',')
+
+			#plot_chs(testing_data.T, 'original_' + selected_orientaion + '_' + mode)
+			#plot_chs(adapted_data_set.T, 'adapted_' + selected_orientaion + '_' + mode)
+
+			# Save somewhere SwRI can access
+			docker_output_dir = '/data/cp2/'
+			os.makedirs(docker_output_dir, exist_ok=True)
+			copyfile(output_file, docker_output_dir + output_file)
 
 
 
